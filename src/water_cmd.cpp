6474814eaffb0ba6c926900dbf47724826ce053d
Michael Lutz <michi@icosahedron.de>
2010-06-30 02:35:13 +0200

    -Codechange: Iterate over all associated tiles when handling a VehicleEnterTile event.

--- src/water_cmd.cpp
+++ src/water_cmd.cpp
@@ -1304,7 +1304,7 @@ static bool ChangeTileOwner_Water(TileIndex tile, Tile *tptr, Owner old_owner, O
 	return true;
 }
 
-static VehicleEnterTileStatus VehicleEnter_Water(Vehicle *v, TileIndex tile, int x, int y)
+static VehicleEnterTileStatus VehicleEnter_Water(Vehicle *v, TileIndex tile, Tile *water_tile, int x, int y)
 {
 	return VETSB_CONTINUE;
 }
