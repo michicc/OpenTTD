163b76e2eb7141cc02593f37e5445d55cc764076
Michael Lutz <michi@icosahedron.de>
2010-07-01 23:02:52 +0200

    -Codechange: Load/save the additional map data as well.

--- src/saveload/map_sl.cpp
+++ src/saveload/map_sl.cpp
@@ -47,169 +47,236 @@ static void Check_MAPS()
 	_load_check_data.map_size_y = _map_dim_y;
 }
 
-static const uint MAP_SL_BUF_SIZE = 4096;
+void Save_MAPR()
+{
+	SlSetLength(MapSizeY() * sizeof(uint) + MapSizeY() * MapSizeX() * sizeof(uint16));
+
+	/* Save length of each map line. */
+	SmallStackSafeStackAlloc<uint, MAX_MAP_SIZE> buf;
+	for (uint i = 0; i < MapSizeY(); i++) buf[i] = _m.tiles[i].Length();
+	SlArray(buf, MapSizeY(), SLE_UINT);
+
+	/* Save offset table. */
+	SlArray(_m.offset, MapSizeX() * MapSizeY(), SLE_UINT16);
+}
+
+void Load_MAPR()
+{
+	SmallStackSafeStackAlloc<uint, MAX_MAP_SIZE> buf;
+
+	/* Resize each map line to the final length. */
+	SlArray(buf, MapSizeY(), SLE_UINT);
+	for (uint i = 0; i < MapSizeY(); i++) _m.tiles[i].Append(buf[i] - _m.tiles[i].Length());
+
+	/* Load offset table. */
+	SlArray(_m.offset, MapSizeX() * MapSizeY(), SLE_UINT16);
+}
+
+static const size_t MAP_SL_BUF_SIZE = 4096;
 
 static void Load_MAPT()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].type_height = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->type_height = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAPT()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].type_height;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->type_height;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP1()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m1 = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->m1 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP1()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m1;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m1;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP2()
 {
 	SmallStackSafeStackAlloc<uint16, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE,
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk,
 			/* In those versions the m2 was 8 bits */
 			IsSavegameVersionBefore(5) ? SLE_FILE_U8 | SLE_VAR_U16 : SLE_UINT16
 		);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m2 = buf[j];
+		for (uint j = 0; j < chunk; ++j, ++i) i->m2 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP2()
 {
 	SmallStackSafeStackAlloc<uint16, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size * sizeof(uint16));
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m2;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT16);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m2;
+		SlArray(buf, chunk, SLE_UINT16);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP3()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m3 = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->m3 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP3()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m3;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m3;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP4()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m4 = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->m4 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP4()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m4;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m4;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP5()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m5 = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->m5 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP5()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m5;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m5;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP6()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	if (IsSavegameVersionBefore(42)) {
-		for (TileIndex i = 0; i != size;) {
-			/* 1024, otherwise we overflow on 64x64 maps! */
-			SlArray(buf, 1024, SLE_UINT8);
-			for (uint j = 0; j != 1024; j++) {
-				_m[i++].m6 = GB(buf[j], 0, 2);
-				_m[i++].m6 = GB(buf[j], 2, 2);
-				_m[i++].m6 = GB(buf[j], 4, 2);
-				_m[i++].m6 = GB(buf[j], 6, 2);
+		Map::Iterator i = _m.Begin();
+		while (size > 0) {
+			size_t chunk = min(size, MAP_SL_BUF_SIZE);
+			/* There are four tiles packed into one byte! */
+			SlArray(buf, chunk / 4, SLE_UINT8);
+			for (uint j = 0; j < chunk / 4; ++j) {
+				(i++)->m6 = GB(buf[j], 0, 2);
+				(i++)->m6 = GB(buf[j], 2, 2);
+				(i++)->m6 = GB(buf[j], 4, 2);
+				(i++)->m6 = GB(buf[j], 6, 2);
 			}
+			size -= chunk;
 		}
 	} else {
-		for (TileIndex i = 0; i != size;) {
-			SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-			for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m6 = buf[j];
+		Map::Iterator i = _m.Begin();
+		while (size > 0) {
+			size_t chunk = min(size, MAP_SL_BUF_SIZE);
+			SlArray(buf, chunk, SLE_UINT8);
+			for (uint j = 0; j < chunk; ++j, ++i) i->m6 = buf[j];
+			size -= chunk;
 		}
 	}
 }
@@ -217,40 +284,50 @@ static void Load_MAP6()
 static void Save_MAP6()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m6;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m6;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 static void Load_MAP7()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
-	for (TileIndex i = 0; i != size;) {
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) _m[i++].m7 = buf[j];
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		SlArray(buf, chunk, SLE_UINT8);
+		for (uint j = 0; j < chunk; ++j, ++i) i->m7 = buf[j];
+		size -= chunk;
 	}
 }
 
 static void Save_MAP7()
 {
 	SmallStackSafeStackAlloc<byte, MAP_SL_BUF_SIZE> buf;
-	TileIndex size = MapSize();
+	size_t size = _m.GetTileCount();
 
 	SlSetLength(size);
-	for (TileIndex i = 0; i != size;) {
-		for (uint j = 0; j != MAP_SL_BUF_SIZE; j++) buf[j] = _m[i++].m7;
-		SlArray(buf, MAP_SL_BUF_SIZE, SLE_UINT8);
+	Map::Iterator i = _m.Begin();
+	while (size > 0) {
+		size_t chunk = min(size, MAP_SL_BUF_SIZE);
+		for (uint j = 0; j < chunk; ++j, ++i) buf[j] = i->m7;
+		SlArray(buf, chunk, SLE_UINT8);
+		size -= chunk;
 	}
 }
 
 extern const ChunkHandler _map_chunk_handlers[] = {
 	{ 'MAPS', Save_MAPS, Load_MAPS, NULL, Check_MAPS, CH_RIFF },
+	{ 'MAPR', Save_MAPR, Load_MAPR, NULL, NULL,       CH_RIFF },
 	{ 'MAPT', Save_MAPT, Load_MAPT, NULL, NULL,       CH_RIFF },
 	{ 'MAPO', Save_MAP1, Load_MAP1, NULL, NULL,       CH_RIFF },
 	{ 'MAP2', Save_MAP2, Load_MAP2, NULL, NULL,       CH_RIFF },
