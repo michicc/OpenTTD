ba14249b7a930535b082af8586966d3e3c54fc3a
Michael Lutz <michi@icosahedron.de>
2018-06-03 23:37:46 +0200

    Codechange: Split tree information from ground information. MP_TREES tiles are now associated tiles of MP_CLEAR or MP_WATER.

--- src/station_cmd.cpp
+++ src/station_cmd.cpp
@@ -190,7 +190,7 @@ static bool CMSAWater(TileIndex tile)
  */
 static bool CMSATree(TileIndex tile)
 {
-	return IsTileType(tile, MP_TREES);
+	return HasTileByType(tile, MP_TREES);
 }
 
 #define M(x) ((x) - STR_SV_STNAME)
