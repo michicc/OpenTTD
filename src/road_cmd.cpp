3802adba1ef1c8c61a1a1b26f9ff3aab1caa227a
Michael Lutz <michi@icosahedron.de>
2018-06-13 23:49:53 +0200

    Codechange: Split rail information from ground information. MP_RAILWAY tiles are now associated tiles of MP_CLEAR or MP_WATER.

--- src/road_cmd.cpp
+++ src/road_cmd.cpp
@@ -35,6 +35,7 @@
 #include "date_func.h"
 #include "genworld.h"
 #include "company_gui.h"
+#include "clear_map.h"
 
 #include "table/strings.h"
 
@@ -378,9 +379,11 @@ static CommandCost RemoveRoad(TileIndex tile, DoCommandFlag flags, RoadBits piec
 				if (rts == ROADTYPES_NONE) {
 					TrackBits tracks = GetCrossingRailBits(tile);
 					bool reserved = HasCrossingReservation(tile);
-					MakeRailNormal(tile, GetTileOwner(tile), tracks, GetRailType(tile));
+					RailType rail_type = GetRailType(tile);
+					Owner rail_o = GetTileOwner(tile);
 
-					Tile *rail_tile = GetTileByType(tile, MP_RAILWAY);
+					MakeClearGrass(tile);
+					Tile *rail_tile = MakeRailNormal(tile, rail_o, tracks, rail_type);
 					if (reserved) SetTrackReservation(rail_tile, tracks);
 
 					/* Update rail count for level crossings. The plain track should still be accounted
@@ -521,6 +524,72 @@ CommandCost CmdBuildRoad(TileIndex tile, DoCommandFlag flags, uint32 p1, uint32 
 	Slope tileh = GetTileSlope(tile);
 
 	bool need_to_clear = false;
+
+	if (HasTileByType(tile, MP_RAILWAY)) {
+		Tile *rail_tile = GetTileByType(tile, MP_RAILWAY);
+
+		if (IsSteepSlope(tileh)) {
+			return_cmd_error(STR_ERROR_LAND_SLOPED_IN_WRONG_DIRECTION);
+		}
+
+		/* Level crossings may only be built on these slopes */
+		if (!HasBit(VALID_LEVEL_CROSSING_SLOPES, tileh)) {
+			return_cmd_error(STR_ERROR_LAND_SLOPED_IN_WRONG_DIRECTION);
+		}
+
+		if (GetRailTileType(rail_tile) != RAIL_TILE_NORMAL) goto do_clear;
+
+		if (RailNoLevelCrossings(GetRailType(rail_tile))) {
+			return_cmd_error(STR_ERROR_CROSSING_DISALLOWED);
+		}
+
+		Axis roaddir;
+		switch (GetTrackBits(rail_tile)) {
+			case TRACK_BIT_X:
+				if (pieces & ROAD_X) goto do_clear;
+				roaddir = AXIS_Y;
+				break;
+
+			case TRACK_BIT_Y:
+				if (pieces & ROAD_Y) goto do_clear;
+				roaddir = AXIS_X;
+				break;
+
+			default: goto do_clear;
+		}
+
+		CommandCost ret = EnsureNoVehicleOnGround(tile);
+		if (ret.Failed()) return ret;
+
+		if (flags & DC_EXEC) {
+			Track railtrack = AxisToTrack(OtherAxis(roaddir));
+			YapfNotifyTrackLayoutChange(tile, railtrack);
+			bool reserved = HasBit(GetRailReservationTrackBits(rail_tile), railtrack);
+			RailType rail_type = GetRailType(rail_tile);
+			Owner rail_o = GetTileOwner(rail_tile);
+
+			/* Update company infrastructure counts. A level crossing has two road bits. */
+			Company *c = Company::GetIfValid(company);
+			if (c != NULL) {
+				c->infrastructure.road[rt] += 2;
+				if (rt != ROADTYPE_ROAD) c->infrastructure.road[ROADTYPE_ROAD] += 2;
+				DirtyCompanyInfrastructureWindows(company);
+			}
+			/* Update rail count for level crossings. The plain track is already
+			 * counted, so only add the difference to the level crossing cost. */
+			c = Company::GetIfValid(rail_o);
+			if (c != NULL) c->infrastructure.rail[rail_type] += LEVELCROSSING_TRACKBIT_FACTOR - 1;
+
+			_m.RemoveTile(tile, rail_tile);
+			/* Always add road to the roadtypes (can't draw without it) */
+			MakeRoadCrossing(tile, company, company, rail_o, roaddir, rail_type, RoadTypeToRoadTypes(rt) | ROADTYPES_ROAD, p2);
+			SetCrossingReservation(tile, reserved);
+			UpdateLevelCrossing(tile, false);
+			MarkTileDirtyByTile(tile);
+		}
+		return CommandCost(EXPENSES_CONSTRUCTION, _price[PR_BUILD_ROAD] * (rt == ROADTYPE_ROAD ? 2 : 4));
+	}
+
 	switch (GetTileType(tile)) {
 		case MP_ROAD:
 			switch (GetRoadTileType(tile)) {
@@ -595,68 +664,6 @@ CommandCost CmdBuildRoad(TileIndex tile, DoCommandFlag flags, uint32 p1, uint32 
 				default: NOT_REACHED();
 			}
 			break;
-
-		case MP_RAILWAY: {
-			if (IsSteepSlope(tileh)) {
-				return_cmd_error(STR_ERROR_LAND_SLOPED_IN_WRONG_DIRECTION);
-			}
-
-			/* Level crossings may only be built on these slopes */
-			if (!HasBit(VALID_LEVEL_CROSSING_SLOPES, tileh)) {
-				return_cmd_error(STR_ERROR_LAND_SLOPED_IN_WRONG_DIRECTION);
-			}
-
-			if (GetRailTileType(_m.ToTile(tile)) != RAIL_TILE_NORMAL) goto do_clear;
-
-			if (RailNoLevelCrossings(GetRailType(tile))) {
-				return_cmd_error(STR_ERROR_CROSSING_DISALLOWED);
-			}
-
-			Axis roaddir;
-			switch (GetTrackBits(tile)) {
-				case TRACK_BIT_X:
-					if (pieces & ROAD_X) goto do_clear;
-					roaddir = AXIS_Y;
-					break;
-
-				case TRACK_BIT_Y:
-					if (pieces & ROAD_Y) goto do_clear;
-					roaddir = AXIS_X;
-					break;
-
-				default: goto do_clear;
-			}
-
-			CommandCost ret = EnsureNoVehicleOnGround(tile);
-			if (ret.Failed()) return ret;
-
-			if (flags & DC_EXEC) {
-				Track railtrack = AxisToTrack(OtherAxis(roaddir));
-				YapfNotifyTrackLayoutChange(tile, railtrack);
-				/* Update company infrastructure counts. A level crossing has two road bits. */
-				Company *c = Company::GetIfValid(company);
-				if (c != NULL) {
-					c->infrastructure.road[rt] += 2;
-					if (rt != ROADTYPE_ROAD) c->infrastructure.road[ROADTYPE_ROAD] += 2;
-					DirtyCompanyInfrastructureWindows(company);
-				}
-				/* Update rail count for level crossings. The plain track is already
-				 * counted, so only add the difference to the level crossing cost. */
-				c = Company::GetIfValid(GetTileOwner(tile));
-				if (c != NULL) {
-					c->infrastructure.rail[GetRailType(tile)] += LEVELCROSSING_TRACKBIT_FACTOR - 1;
-					DirtyCompanyInfrastructureWindows(c->index);
-				}
-
-				/* Always add road to the roadtypes (can't draw without it) */
-				bool reserved = HasBit(GetRailReservationTrackBits(_m.ToTile(tile)), railtrack);
-				MakeRoadCrossing(tile, company, company, GetTileOwner(tile), roaddir, GetRailType(tile), RoadTypeToRoadTypes(rt) | ROADTYPES_ROAD, p2);
-				SetCrossingReservation(tile, reserved);
-				UpdateLevelCrossing(tile, false);
-				MarkTileDirtyByTile(tile);
-			}
-			return CommandCost(EXPENSES_CONSTRUCTION, _price[PR_BUILD_ROAD] * (rt == ROADTYPE_ROAD ? 2 : 4));
-		}
 
 		case MP_STATION: {
 			if ((GetAnyRoadBits(tile, rt) & pieces) == pieces) return_cmd_error(STR_ERROR_ALREADY_BUILT);
