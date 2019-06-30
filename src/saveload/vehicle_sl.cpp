0a9941d52d5a21ad65f3e5bfa4c417d9081e7fb3
Michael Lutz <michi@icosahedron.de>
2019-06-30 15:03:28 +0200

    fixup! Codechange: Keep the last station a vehicle loaded at and the OrderID which caused the vehicle to arrive there.

--- src/saveload/vehicle_sl.cpp
+++ src/saveload/vehicle_sl.cpp
@@ -632,7 +632,7 @@ const SaveLoad *GetVehicleDescription(VehicleType vt)
 		 SLE_CONDVAR(Vehicle, last_station_visited,  SLE_FILE_U8  | SLE_VAR_U16,   SL_MIN_VERSION,   SLV_5),
 		 SLE_CONDVAR(Vehicle, last_station_visited,  SLE_UINT16,                   SLV_5, SL_MAX_VERSION),
 		 SLE_CONDVAR(Vehicle, last_loading_station,  SLE_UINT16,                 SLV_182, SL_MAX_VERSION),
-		 SLE_CONDVAR(Vehicle, last_loading_order,    SLE_UINT16,                SLV_YACD, SL_MAX_VERSION),
+		 SLE_CONDVAR(Vehicle, last_loading_order,    SLE_UINT32,                SLV_YACD, SL_MAX_VERSION),
 
 		     SLE_VAR(Vehicle, cargo_type,            SLE_UINT8),
 		 SLE_CONDVAR(Vehicle, cargo_subtype,         SLE_UINT8,                   SLV_35, SL_MAX_VERSION),
@@ -666,7 +666,7 @@ const SaveLoad *GetVehicleDescription(VehicleType vt)
 		 SLE_CONDVAR(Vehicle, current_order.type,    SLE_UINT8,                    SLV_5, SL_MAX_VERSION),
 		 SLE_CONDVAR(Vehicle, current_order.flags,   SLE_UINT8,                    SLV_5, SL_MAX_VERSION),
 		 SLE_CONDVAR(Vehicle, current_order.dest,    SLE_UINT16,                   SLV_5, SL_MAX_VERSION),
-		 SLE_CONDVAR(Vehicle, current_order.index,   SLE_UINT16,                SLV_YACD, SL_MAX_VERSION),
+		 SLE_CONDVAR(Vehicle, current_order.index,   SLE_UINT32,                SLV_YACD, SL_MAX_VERSION),
 
 		/* Refit in current order */
 		 SLE_CONDVAR(Vehicle, current_order.refit_cargo,   SLE_UINT8,             SLV_36, SL_MAX_VERSION),
