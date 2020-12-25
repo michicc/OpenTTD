3b7764fe4c0120e1d3b8d6bfb480d19ee36f5d29
Michael Lutz <michi@icosahedron.de>
2020-12-26 00:19:50 +0100

    Add: [OpenGL] Accelerated mouse cursor drawing.

--- src/video/win32_v.h
+++ src/video/win32_v.h
@@ -67,6 +67,8 @@ protected:
 	virtual void Paint(HWND hWnd, bool in_sizemove) = 0;
 	/** Thread function for threaded drawing. */
 	virtual void PaintThread() = 0;
+	/** Draw the mouse cursor. */
+	virtual void DrawMouseCursor();
 	/** Lock video buffer for drawing if it isn't already mapped. */
 	virtual bool LockVideoBuffer();
 	/** Unlock video buffer. */
@@ -139,6 +141,13 @@ public:
 
 	bool HasEfficient8Bpp() const override { return true; }
 
+	bool UseSystemCursor() override
+	{
+		return true;
+	}
+
+	void ClearSystemSprites() override;
+
 	const char *GetName() const override { return "win32-opengl"; }
 
 protected:
@@ -154,6 +163,7 @@ protected:
 	void PaletteChanged(HWND hWnd) override;
 	void Paint(HWND hWnd, bool in_sizemove) override;
 	void PaintThread() override {}
+	void DrawMouseCursor() override;
 
 	const char *AllocateContext();
 	void DestroyContext();
