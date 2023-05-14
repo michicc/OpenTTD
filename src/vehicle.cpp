3777f452149d4e6dd162b43b2cc192c60cf299cf
Michael Lutz <michi@icosahedron.de>
2023-05-14 22:23:53 +0200

    Codechange: Change OrderBackup to operates on consists and not vehicles.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -864,7 +864,6 @@ void Vehicle::PreDestructor()
 		CloseWindowById(WC_VEHICLE_DETAILS, this->index);
 		CloseWindowById(WC_VEHICLE_TIMETABLE, this->index);
 		SetWindowDirty(WC_COMPANY, this->owner);
-		OrderBackup::ClearVehicle(this);
 	}
 	InvalidateWindowClassesData(GetWindowClassForVehicleType(this->type), 0);
 
